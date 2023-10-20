c292c8d109a558dd0023f0842448e482cfeb382b
23P31A224 <147792019+23P31A224@users.noreply.github.com>
2023-10-20 10:31:14 +0530

    #include<stdio.h> int main() {     int a,b,res;     scanf("%d %d",&a,&b);     if(res=a-b)     {         printf("%d",res);     }     else     {         printf("0");     } }

---
+++ Chairs_Requirement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,res;
+    scanf("%d %d",&a,&b);
+    if(res=a-b)
+    {
+        printf("%d",res);
+    }
+    else
+    {
+        printf("0");
+    }
+}
