30ad341ea80f1fab2246393802beaa4db5576406
Likhitha G <147792019+23P31A224@users.noreply.github.com>
2024-03-07 21:25:37 +0530

    #include<stdio.h> #include<ctype.h> int main() {     char str[100];     int lowercaseCount=0;     fgets(str,sizeof(str),stdin);     for(int i=0;str[i]!=NULL;i++)     {         if(islower(str[i]))         {             lowercaseCount++;         }     }     printf("%d ",lowercaseCount); }

---
+++ Lowercase_Letters_in_a_String.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char str[100];
+    int lowercaseCount=0;
+    fgets(str,sizeof(str),stdin);
+    for(int i=0;str[i]!=NULL;i++)
+    {
+        if(islower(str[i]))
+        {
+            lowercaseCount++;
+        }
+    }
+    printf("%d
+",lowercaseCount);
+}
